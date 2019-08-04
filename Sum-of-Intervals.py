4c6e4291aaff75e7ca7e451966fdc709b2a69cc3
nandishpatel1996 <1418beit30031@gmail.com>
2019-08-04 15:32:44 +1000

    update

--- Sum-of-Intervals.py
+++ Sum-of-Intervals.py
@@ -2,13 +2,7 @@ def sum_of_intervals(intervals):
 	final_list = []
 	for interval in intervals:
 		start,end = interval
-		final_list.extend(list(range(start,end)))
+		final_list.extend(range(start,end))
 	return len(set(final_list))
 
-print(sum_of_intervals( [
-   [1,5],
-   [10, 20],
-   [1, 6],
-   [16, 19],
-   [5, 11]
-] ))
+print(sum_of_intervals([(1, 4), (7, 10), (3, 5)]))
