b10779b830aef83e11ae4e0afeeff45feec918fa
nandishpatel1996 <1418beit30031@gmail.com>
2019-08-17 20:49:03 +1000

    Hello World - Without Strings.py

---
+++ Hello World - Without Strings.py
@@ -0,0 +1,8 @@
+def hello_world():
+	final_string = str()
+	asciiiiiiiiii = [72, 101, 108, 108, 111, 44, 32, 87, 111, 114, 108, 100, 33]
+	for i in asciiiiiiiiii:final_string += chr(i)
+	return final_string
+
+
+print(hello_world())
