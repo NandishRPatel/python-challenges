7491bc4fe0b2828433bc41a3ca8991d1752d5535
nandishpatel1996 <1418beit30031@gmail.com>
2019-09-01 21:11:40 +1000

    tortoise racing changed

--- tortoise-racing.py
+++ tortoise-racing.py
@@ -1,17 +1,17 @@
 def race(v1, v2, g):
-    l=[]
-    x=v2-v1
-    if v1>v2:
+    l = []
+    x = v2 - v1
+    if v1 > v2:
         return
-    h=g//x
-    m=(g/x-h)*60
-    s=(m-int(m))*60
-    if m>59:    
-        h+=1
-    if s>59:
-        m+=1
-        s=0
-    
-    
-    l.append(int(h));l.append(int(m));l.append(int(s))
+    h = g // x
+    m = (g / x - h) * 60
+    s = (m - int(m)) * 60
+    if m > 59:    
+        h += 1
+    if s > 59:
+        m += 1
+        s = 0
+    l.append(int(h))
+    l.append(int(m))
+    l.append(int(s))
     return l
