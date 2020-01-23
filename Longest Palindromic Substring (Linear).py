d45c2bf705b79e822387569339ddd121d24096b6
nandishpatel1996 <1418beit30031@gmail.com>
2020-01-23 21:58:09 +1100

    Longest pallindrome

--- Longest Palindromic Substring (Linear).py
+++ Longest Palindromic Substring (Linear).py
@@ -7,16 +7,47 @@
 # TODO: Complete in linear time xDD
 def longest_palindrome(s):
 	if not s: return s
+	elif len(set(s)) == 1: return s
 	else:
-		string = s[0]
+		len_s = len(s)
+		new = ""
 		pallindrome = ""
-		for i in s[1:]:
-			length = len(pallindrome)
-			string += i
-			if string == string[::-1]:
-				if len(string) > length:
-					pallindrome = string
+		length = 0
+		for i in range(0, len_s):
+			for j in range(len_s - 1, i + length - 1, -1):
+				if s[i] == s[j]:
+					new = s[i:j + 1]
+					if new == new[::-1]:
+						if length < len(new):
+							pallindrome = new
+							length = len(pallindrome)
+		
+		if pallindrome:return pallindrome
+		else: return s[0]
+			
 
-		return pallindrome
+'''print(longest_palindrome('babad') == 'bab')
+print(longest_palindrome('madam') == 'madam')
+print(longest_palindrome('dde') == 'dd')
+print(longest_palindrome('ababbab') == 'babbab')
+print(longest_palindrome('abababa') == 'abababa')
 
-print(longest_palindrome("cbbd"))
+
+print(longest_palindrome('banana') == 'anana')
+print(longest_palindrome('abba') == 'abba')
+print(longest_palindrome('cbbd') == 'bb')
+print(longest_palindrome('zz') == 'zz')
+print(longest_palindrome('dddd') == 'dddd')
+
+
+print(longest_palindrome('') == '')
+print(longest_palindrome('abcdefghijklmnopqrstuvwxyz') == 'a')
+print(longest_palindrome('ttaaftffftfaafatf') == 'aaftffftfaa')
+
+print(longest_palindrome('bbaaacc') == 'aaa')
+print(longest_palindrome('m') == 'm')
+'''
+import time
+s = time.time()
+print(longest_palindrome('a'*10000) == 'a'*10000)
+print(time.time()-s)
